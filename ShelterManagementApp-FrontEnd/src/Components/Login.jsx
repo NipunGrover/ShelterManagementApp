2c36faffbe3a1ef668fc26fb0f82dfdb5738b8fd
NipunGrover <ngrover5702@conestogac.on.ca>
2024-01-18 01:00:20 -0500

    created a new component login and removed the default app content

---
+++ ShelterManagementApp-FrontEnd/src/Components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState, useRef, useEffect } from 'react'
+
+export const Login = () => {
+  const userRef = useRef();
+  const errorRef = useRef();
+  
+  const [user, setUser] = useState('');   
+
+
+  return (
+
+    <div>Login</div>
+  )
+}
