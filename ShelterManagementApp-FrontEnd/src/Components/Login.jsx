4bb9008aeea3023385bdcb277ae6a4380b7867d9
NipunGrover <ngrover5702@conestogac.on.ca>
2024-01-18 02:50:31 -0500

    added form to the Login  page, added css styling, used a template and adjusted according to preferences

--- ShelterManagementApp-FrontEnd/src/Components/Login.jsx
+++ ShelterManagementApp-FrontEnd/src/Components/Login.jsx
@@ -6,10 +6,64 @@ export const Login = () => {
   const errorRef = useRef();
   
   const [user, setUser] = useState('');   
+  const [password, setPasswword] = useState('');
+  const [errorMsg, setErrorMsg] = useState(false);  
+  const [successMsg, setSuccessMsg] = useState(false);  
 
+  useEffect(() => {
+    userRef.current.focus();
+  } ,[]);
+
+
+  //this is to make the error disappear when  the user types again as they are adjusting the input
+  useEffect(() => { 
+    setErrorMsg('');
+  }, [user, password]);
+
+//this is to prevent the form from submitting
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+  }
 
   return (
 
-    <div>Login</div>
+    <section>
+      <p ref={errorRef} className={errorMsg ? "errorMsg" : "offscreen"} 
+      aria-live="assertive"> {errorMsg} </p>
+      <h1>Sign In</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+          <input 
+            type="text"
+            id = "username"
+            ref = {userRef}
+            autoComplete="off"
+            //to track if the username typed is valid
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+          />
+
+        <label htmlFor="password">Password</label>
+          <input 
+            type="password"
+            id = "password"
+            autoComplete="off"
+            //to track if the username typed is valid
+            onChange={(e) => setUser(e.target.value)}
+            value={password}
+            required
+          />
+          <button>Sign In</button>
+      </form>
+      <p>
+        Need an Account? <br/>
+        <span className="line">
+          {""}
+          <a href="#">Sign Up</a>
+        </span>
+      </p>
+    </section>
   )
 }
